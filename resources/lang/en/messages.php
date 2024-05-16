08559303b032dd7851ba56990e55c29fa052aee0
rashminkachhadiya <70191122+rashminkachhadiya@users.noreply.github.com>
2024-05-16 21:08:10 +0530

    Add files via upload

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,124 @@
+<?php
+
+return [
+
+    /** All **/
+    'InputText' => 'Please write something here...',
+    'cacheReloaded' => 'Information has been reloaded.',
+
+    /** Clients Messages **/
+    'SuccessClientStore' => 'Client has been added.',
+    'ErrorClientStore' => 'Client hasn\'t been added yet.',
+    'SuccessClientUpdate' => 'Client has been updated.',
+    'ErrorClientUpdate' => 'Client hasn\'t been updated yet.',
+    'SuccessClientDelete' => 'Client has been deleted.',
+    'SuccessClientActive' => 'Client has been active.',
+    'ClientIsActivated' => 'Client has been already activated.',
+    'ErrorClientActive' => 'Client hasn\'t been active yet.',
+    'ClientIsNowDeactivated' => 'Client has been deactivated.',
+    'ClientIsDeactivated' => 'Client has been already deactivated.',
+    'ThereIsNoClient' => 'A client with this name doesn\'t exist.',
+    'SuccessSettingsUpdate' => 'Settings has been updated.',
+    'ErrorSettingsUpdate' => 'There is problem with settings update.',
+
+    /** Companies Messages **/
+    'SuccessCompaniesStore' => 'Company has been added.',
+    'ErrorCompaniesStore' => 'Company hasn\'t been added yet.',
+    'SuccessCompaniesUpdate' => 'Company has been updated.',
+    'ErrorCompaniesUpdate' => 'Company hasn\'t been updated yet.',
+    'SuccessCompaniesDelete' => 'Company has been deleted.',
+    'SuccessCompaniesActive' => 'Company has been active.',
+    'ErrorCompaniesActive' => 'Company hasn\'t been active yet.',
+    'CompaniesIsNowDeactivated' => 'Company has been deactivated.',
+    'CompaniesIsDeactivated' => 'Company has been already deactivated.',
+    'ThereIsNoCompanies' => 'A company with this name doesn\'t exist.',
+    'firstDeleteCompanies' => 'Cannot delete this company, have assigned companies.',
+    'firstDeleteEmployees' => 'Cannot delete this company, have assigned employees.',
+    'firstDeleteDeals' => 'Cannot delete company, have assigned deals.',
+
+
+
+    /** Contacts Messages **/
+    'SuccessContactsStore' => 'Contact has been added.',
+    'ErrorContactsStore' => 'Contact hasn\'t been added yet.',
+    'SuccessContactsUpdate' => 'Contact has been updated.',
+    'ErrorContactsUpdate' => 'Contact hasn\'t been updated yet.',
+    'SuccessContactsDelete' => 'Contact has been deleted.',
+    'SuccessContactsActive' => 'Contact has been active.',
+    'ErrorContactsActive' => 'Contact hasn\'t been active yet.',
+    'ContactsIsNowDeactivated' => 'Contact has been deactivated.',
+    'ContactsIsDeactivated' => 'Contact has been already deactivated.',
+
+    /** Deals Messages **/
+    'SuccessDealsStore' => 'Deal has been added.',
+    'SuccessDealTermStore' => 'Deal term has been added.',
+    'ErrorDealTermStore' => 'Deal term hasn\'t been added yet.',
+    'ErrorDealsStore' => 'Deal hasn\'t been added yet.',
+    'SuccessDealsUpdate' => 'Deal has been updated.',
+    'ErrorDealsUpdate' => 'Deal hasn\'t been updated yet.',
+    'SuccessDealsDelete' => 'Deal has been deleted.',
+    'SuccessDealsTermDelete' => 'Deal term has been deleted.',
+    'SuccessDealsActive' => 'Deal has been active.',
+    'ErrorDealsActive' => 'Deal hasn\'t been active yet.',
+    'DealsIsNowDeactivated' => 'Deal has been deactivated.',
+    'DealsIsDeactivated' => 'Deal has been already deactivated.',
+    'ThereIsNoDeals' => 'A deal with this name doesn\'t exist.',
+    'SuccessDealsTermsStore' => 'Deal terms has been stored.',
+    'firstDeleteDealTerms' => 'Please delete first deal terms.',
+
+    /** Employees Messages **/
+    'SuccessEmployeesStore' => 'Employee has been added.',
+    'ErrorEmployeesStore' => 'Employee hasn\'t been added yet.',
+    'SuccessEmployeesUpdate' => 'Employee has been updated.',
+    'ErrorEmployeesUpdate' => 'Employee hasn\'t been updated yet.',
+    'SuccessEmployeesDelete' => 'Employee has been deleted.',
+    'SuccessEmployeesActive' => 'Employee has been active.',
+    'ErrorEmployeesActive' => 'Employee hasn\'t been active yet.',
+    'EmployeesIsNowDeactivated' => 'Employee has been deactivated.',
+    'EmployeesIsDeactivated' => 'Employee has been already deactivated.',
+    'firstDeleteTasks' => 'You cannot delete this employee that has been assigned a tasks.',
+
+
+
+    /** Tasks Messages **/
+    'TasksCompleted' => 'Task successfully completed.',
+    'TasksunCompleted' => 'Task successfully uncompleted.',
+    'SuccessTasksDelete' => 'Task has been deleted.',
+    'SuccessTasksStore' => 'Task has been added.',
+    'SuccessTasksUpdate' => 'Task has been updated.',
+    'SuccessTasksActive' => 'Task has been active.',
+    'TaskIsNowDeactivated' => 'Task has been deactivated.',
+    'ThereIsNoTasks' => 'A task with this name doesn\'t exist.',
+    'CantDeleteUnompletedTask' => 'Task hasn\'t been completed yet.',
+
+    /** Sales Messages **/
+    'SuccessSalesStore' => 'Sales has been added.',
+    'SuccessSalesDelete' => 'Sales has been deleted.',
+    'SuccessSalesUpdate' => 'Sales has been updated.',
+    'SuccessSalesActive' => 'Sales has been active.',
+    'SalesIsNowDeactivated' => 'Sales has been deactivated.',
+    'SalesIsDeactivated' => 'Sales has been already deactivated.',
+    'ErrorSalesActive' => 'Sales hasn\'t been active yet.',
+
+    /** Finances Messages **/
+    'SuccessFinancesStore' => 'Finances has been added.',
+    'SuccessFinancesDelete' => 'Finances has been deleted.',
+    'SuccessFinancesUpdate' => 'Finances has been updated.',
+    'SuccessFinancesActive' => 'Finances has been active.',
+    'ErrorFinancesActive' => 'Finances hasn\'t been active yet.',
+    'FinancesIsNowDeactivated' => 'Finances has been deactivated.',
+    'FinancesIsDeactivated' => 'Finances has been already deactivated.',
+
+    /** Products Messages **/
+    'SuccessProductsStore' => 'Product has been added.',
+    'SuccessProductsDelete' => 'Product has been deleted.',
+    'SuccessProductsUpdate' => 'Product has been updated.',
+    'SuccessProductsActive' => 'Product has been active.',
+    'ErrorProductsActive' => 'Product hasn\'t been active yet.',
+    'ProductsIsNowDeactivated' => 'Product has been deactivated.',
+    'ProductsIsDeactivated' => 'Product has been already deactivated.',
+    'firstDeleteSales' => 'You cannot delete this product that has been assigned a sale.',
+
+    /** Admin operations **/
+    'changeAdminPassword' => 'Your password has been changed.',
+];
